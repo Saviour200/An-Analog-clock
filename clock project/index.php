dfbce717b7d50b5e79b06ae19c198660b779f530
Iorhee .S. Saviour <75181227+Saviour200@users.noreply.github.com>
2024-11-24 23:17:28 +0100

    Add files via upload

---
+++ clock project/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Unique Clock</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="clock">
+    <div class="center-circle"></div>
+    <div class="numbers">
+      <div class="number" style="--rotate: 30;">1</div>
+      <div class="number" style="--rotate: 60;">2</div>
+      <div class="number" style="--rotate: 90;">3</div>
+      <div class="number" style="--rotate: 120;">4</div>
+      <div class="number" style="--rotate: 150;">5</div>
+      <div class="number" style="--rotate: 180;">6</div>
+      <div class="number" style="--rotate: 210;">7</div>
+      <div class="number" style="--rotate: 240;">8</div>
+      <div class="number" style="--rotate: 270;">9</div>
+      <div class="number" style="--rotate: 300;">10</div>
+      <div class="number" style="--rotate: 330;">11</div>
+      <div class="number" style="--rotate: 0;">12</div>
+    </div>
+    <div class="hands">
+      <div class="hour-hand"></div>
+      <div class="minute-hand"></div>
+      <div class="second-hand"></div>
+    </div>
+    <div class="center-text">
+      <p>Made with Love ❤️</p>
+      <p>by Nathan</p>
+    </div>
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
